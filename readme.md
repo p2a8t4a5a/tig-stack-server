227970693c8e0e43c3a7ddc2b4f45b792218e08b
Carver Haines <carver9@gmail.com>
2019-08-03 16:38:43 -0500

    Added custom retention policy, made setup  script verbose, updated instructions

--- readme.md
+++ readme.md
@@ -9,15 +9,17 @@ cd tig-stack-server
 
 
 #### 2. Setup DB Credentials
-Copy the example database credential file to `dbcreds` and enter your desired InfluxDB admin and telegraf users' passwords.
+Copy the example database credential file to `dbcreds` and enter your desired InfluxDB admin and telegraf users' passwords. Also set an integer for the number of days you want data to be stored in the database. By default it is 90 days. Increasing it beyond 90 days will cause very large amounts of data, you may want to look into Downsampling: https://docs.influxdata.com/influxdb/latest/guides/downsampling_and_retention/
 ```
 cp dbcreds.example dbcreds
 vim dbcreds
 ```
 
 #### 3. Run the InfluxDB Setup script
-This script will start a docker Influx DB container, create a database named `telgraf`, with `admin` and `telegraf` users using the passwords you previously set.
-It will then run the InfluxDB setup.
+**Run this only once to initalize the folders and database**
+
+This script will create grafana and influxdb data folders for volumes in the current directory, initializing InfluxDB in a docker container, create a database named `telgraf`, with `admin` and `telegraf` users using the passwords you previously set. It
+It will then run the InfluxDB setup. Then it will set the retention policy to the number of days specified in the dbcreds file.
 ```
 chmod +x setup-influxdb.sh
 ./setup-influxdb.sh
@@ -28,3 +30,6 @@ This will start grafana listening on port 3000, and InfluxDB on port 8086.
 ```
 docker-compose up -d
 ```
+
+#### 5. Access and setup grafana
+* Grafana available at: https://yourip:3000
--- setup-influxdb.sh
+++ setup-influxdb.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+echo
+echo -e "\033[1mStarting Setup...\033[0m"
+echo
+
 # Make persistant volumes
+echo
+echo -e "\033[1mCreating volumes...\033[0m"
+echo
 mkdir ./grafana-volume
 mkdir ./influxdb-volume
 
 # Grafana folder must have UID/GID perms of '472' to be writeable
+echo
+echo -e "\033[1mChanging folder permissions for Grafana...\033[0m"
+echo
 sudo chown -R 472:472 grafana-volume
 
 # Source the passwords
+echo
+echo -e "\033[1mSourcing data in 'dbcreds'...\033[0m"
+echo
 source dbcreds
 
 # Run the docker once to setup the database and users in the local influxdb folder/volume
+echo
+echo -e "\033[1mInitializing InfluxDB and creating 'telegraf' database...\033[0m"
+echo
+
 docker run --rm \
   -e INFLUXDB_DB=telegraf \
   -e INFLUXDB_ADMIN_ENABLED=true \
@@ -21,6 +38,51 @@ docker run --rm \
   -v $(pwd)/influxdb-volume:/var/lib/influxdb \
   influxdb /init-influxdb.sh
 
+echo
+echo -e "\033[1mFinished initializing database\033[0m"
+echo
+
+# Show current retention policy
+echo
+echo -e "\033[1mPrinting current DB retention policy...\033[0m"
+echo
+
+docker run -d \
+  --name influxdb \
+  -v $(pwd)/influxdb-volume:/var/lib/influxdb \
+  influxdb > /dev/null
+
+docker exec -it \
+  influxdb influx -execute "SHOW RETENTION POLICIES ON telegraf" \
+  | grep true
+echo
+
+# Set retention policy
+echo
+echo -e "\033[1mSetting new $RETENTIONDAYS day retention policy...\033[0m"
+echo
+docker exec -it \
+  influxdb influx -execute "CREATE RETENTION POLICY default_policy ON telegraf DURATION ${RETENTIONDAYS}d REPLICATION 1 DEFAULT"
+
+
+# Show new retention policy
+echo
+echo -e "\033[1mPrinting new DB retention policy...\033[0m"
+echo
+
+docker exec -it \
+  influxdb influx -execute "SHOW RETENTION POLICIES ON telegraf" \
+  | grep true
+echo
+
+docker rm -f influxdb > /dev/null
+
 #Remove secret password variables
+echo
+echo -e "\033[1mCleaning up secrets\033[0m"
+echo
 IFDBAPW="resetpw"
 IFDBTPW="resetpw"
+
+echo -e "\033[1mFinished!\033[0m"
+echo
